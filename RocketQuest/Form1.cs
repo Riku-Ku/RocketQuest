520d33dc30bc3f8a2eb034de08b8841b7c3606a8
Riku-Ku <dmitriy1998r@gmail.com>
2021-06-05 15:31:23 +0500

    Added fullscreen startup

--- RocketQuest/Form1.cs
+++ RocketQuest/Form1.cs
@@ -10,11 +10,27 @@ using System.Windows.Forms;
 
 namespace RocketQuest
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            Point exitBTNLocation = new Point(this.Width - exitBtn.Width, 0);
+            exitBtn.Location = exitBTNLocation;
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            RocketAppClosing();
+        }
+
+        private void RocketAppClosing()
+        {
+            this.Close();
         }
         //Reject humanity, return to monkey
         //hi
