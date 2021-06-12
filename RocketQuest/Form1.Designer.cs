7b2224dc30d1943379186ecbfca75420dbe5080b
Riku-Ku <dmitriy1998r@gmail.com>
2021-06-12 20:27:23 +0500

    Добавлен прототип управления

--- RocketQuest/Form1.Designer.cs
+++ RocketQuest/Form1.Designer.cs
@@ -29,39 +29,25 @@ namespace RocketQuest
         /// </summary>
         private void InitializeComponent()
         {
-            this.exitBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // exitBtn
-            // 
-            this.exitBtn.Location = new System.Drawing.Point(605, 12);
-            this.exitBtn.Name = "exitBtn";
-            this.exitBtn.Size = new System.Drawing.Size(75, 23);
-            this.exitBtn.TabIndex = 0;
-            this.exitBtn.Text = "Χ";
-            this.exitBtn.UseVisualStyleBackColor = true;
-            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(692, 459);
-            this.Controls.Add(this.exitBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.KeyPreview = true;
             this.Name = "MainWindow";
             this.Text = "Title";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.MainWindow_Load);
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainWindow_KeyDown);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainWindow_Paint);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button exitBtn;
     }
 }
 
