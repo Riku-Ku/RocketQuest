b7ca61708384bf7e3a07e6b000656b57f27f2319
grron <grron@OLEGPC>
2021-06-12 18:27:37 +0500

    добавили хитбокс начали делать евент на кнопку

---
+++ RocketQuest/HitBox.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+
+namespace RocketQuest
+{
+    class HitBox
+    {
+        private Point coordinates=new Point(0,0);
+        private int width;
+        private int height;
+        public int SetWidth
+        { 
+            get => width; 
+            set 
+            {
+                if (value >= 0)
+                    width = value;
+            } 
+        }
+        public int SetHeight
+        {
+            get => height;
+            set
+            {
+                if (value >= 0)
+                    height = value;
+            }
+        }
+        public Point SetPoint
+        {
+            get => coordinates;
+            set
+            {
+                coordinates = value;
+            }
+        }
+    }
+}
